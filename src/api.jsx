b4c3b719e120f13a912ab5e94be73e3743e2570e
nitinahlawat12 <130631535+nitinahlawat12@users.noreply.github.com>
2024-09-15 23:58:17 +0530

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000";
+// Use the environment variable for the API URL or default to localhost for development
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
+// API calls
 export const getFAQs = async () => await fetch(`${API_URL}/faqs`);
 export const createFAQ = async (data) => await axios.post(`${API_URL}/faqs`, data);
 export const updateFAQ = async (id, data) => await axios.put(`${API_URL}/faqs/${id}`, data);
